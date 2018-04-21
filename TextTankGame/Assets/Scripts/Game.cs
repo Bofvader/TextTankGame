d866bb18e46a6ac8254b936e53e44fbe9f7729bd
bdorman <bdorman@student.neumont.edu>
2018-04-21 17:59:37 -0600

    Swap world to game, and fixed console model issue

--- TextTankGame/Assets/Scripts/Game.cs
+++ TextTankGame/Assets/Scripts/Game.cs
@@ -2,19 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Game : Singleton<Game> {
+public class Game : Singleton<Game>
+{
 
-    public GameObject player;
-    public GameObject ai;
-    public Tank tankInfo;
+	[SerializeField] Tank[] m_actors;
+	[SerializeField] float m_gravity = 9.8f;
+	[SerializeField] float m_scale = 1.0f;
+
+	public float Gravity { get { return m_gravity; } }
+	public float Scale { get { return m_scale; } }
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+
+	}
+
+	void CollisionDetection()
+	{
+		int i = 0;
+		foreach (Tank t in m_actors)
+		{
+			int j = 0;
+			foreach (Tank o in m_actors)
+			{
+				if (i != j && t.Colliding(o.gameObject))
+				{
+					t.Collision();
+					o.Collision();
+				}
+			}
+		}
+	}
+
+	public void QuiteToMenu()
+	{
+
 	}
 }
