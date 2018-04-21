4b23aa99ce8abc4425795de9b37fc9b11b832bf5
bdorman <bdorman@student.neumont.edu>
2018-04-21 03:42:52 -0600

    Started on the console script

---
+++ TextTankGame/Assets/Scripts/Console.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Console : MonoBehaviour
+{
+	[SerializeField] Text m_log = null;
+	[SerializeField] Text m_input = null;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.KeypadEnter))
+		{
+			string input = m_input.text;
+
+			if(input.Equals("Scan"))
+			{
+				//call player.scan
+			}
+			else
+			{
+				Regex aim = new Regex();
+
+			}
+		}
+	}
+}
