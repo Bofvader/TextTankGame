a4b12ced0cf9aa1af21d52349df2b1916d780086
bdorman <bdorman@student.neumont.edu>
2018-04-21 05:53:46 -0600

    Started on the AI, finished most of the console

--- TextTankGame/Assets/Scripts/AI.cs
+++ TextTankGame/Assets/Scripts/AI.cs
@@ -2,15 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AI : MonoBehaviour {
+public class AI : MonoBehaviour
+{
+	[SerializeField] GameObject m_target = null;
+	[SerializeField] Vector3 m_waypoint;
+	[SerializeField] float m_baseSpeed = 1.0f;
+	[SerializeField] float m_damage = 1.0f;
+	[SerializeField] float m_health = 1.0f;
+	[SerializeField] float m_shotTime = 1.0f;
+	[SerializeField] float m_length = 1.0f;
 
-	// Use this for initialization
-	void Start () {
-		
+	bool isAlive = false;
+	bool m_isFiring = false;
+	float m_speed = 0.0f;
+	float m_shotTimer = 0.0f;
+
+	public float Length { get { return m_length; } }
+
+	private void Start()
+	{
+		m_speed = m_baseSpeed;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		if(isAlive)
+		{
+			Vector3 direction = m_waypoint - transform.position;
+			Vector3 velocity = direction.normalized * m_speed * Time.deltaTime;
+
+			if(m_isFiring)
+			{
+				m_shotTimer += Time.deltaTime;
+			}
+		}
+	}
+
+	public void Fire()
+	{
+		if(m_target)
+		{
+			m_speed = 0.0f;
+			m_isFiring = true;
+			Vector3 direction = m_target.transform.position - transform.position;
+
+			if(m_shotTimer >= m_shotTime)
+			{
+				//Bang
+			}
+		}
 	}
 }
--- TextTankGame/Assets/Scripts/Console.cs
+++ TextTankGame/Assets/Scripts/Console.cs
@@ -8,59 +8,85 @@ using UnityEngine.UI;
 public class Console : MonoBehaviour
 {
 	[SerializeField] Text m_log = null;
-	[SerializeField] Text m_input = null;
+	[SerializeField] InputField m_input = null;
 
+	string m_fireString = @"^(?isx)fire&";
+	string m_scanString = @"^(?isx)scan&";
 	string m_aimString = @"^(?isx)Angle(?<amount>{|0?[1-9]|[1-9][0-9]|1[0-7][0-9]|})$";
 	string m_turnString = @"^(?isx)turn(?<amount>{|0?[1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-9][0-9]|3[0-5][0-9]|})$";
 	string m_moveString = @"^(?isx)move({|north|east|south|west|})({|0?[1-9]|[1-9][0-9]|})({|doubletime|tripletime|halftime|})?$";
+
+	public static string Msg { get; set; }
 
 	// Update is called once per frame
 	void Update()
 	{
 		Debug.Log("Updating");
-		if(Input.GetKeyDown(KeyCode.Return))
+		if (Input.GetKeyDown(KeyCode.Return))
 		{
-			string input = m_input.text;
-			m_input.text.Remove(0, input.Length);
+			string input = m_input.textComponent.text;
+			input = input.Trim();
 
-			if(input.Equals("Scan"))
+			Match match = Regex.Match(input, m_scanString);
+			if (match.Success)
 			{
+				input += "\nScanning...";
 				//call player.scan
+				//input += Msg;
 				Debug.Log("Scanning");
-			}
-			else if(input.Equals("Fire"))
-			{
-				//call player.fire
-				Debug.Log("Firing");
 			}
 			else
 			{
-				Match match = Regex.Match(input, m_aimString);
-				if(match.Success)
-				{
-					//call player.angle(match.Groups[1]);
-				}
-
-				match = Regex.Match(input, m_turnString);
+				match = Regex.Match(input, m_fireString);
 				if (match.Success)
 				{
-					//call player.turn(match.Groups[1]);
+					input += "\nFiring...";
+					//call player.fire
+					//input += Msg;
+					Debug.Log("Firing");
 				}
-
-				match = Regex.Match(input, m_moveString);
-				if(match.Success)
+				else
 				{
-					if (match.Groups.Count == 5)
+					match = Regex.Match(input, m_aimString);
+					if (match.Success)
 					{
-						//call player.move(match.Groups[1], match.Groups[2], match.Groups[3]);
+						//call player.angle(match.Groups[1]);
+						input += "...";
 					}
 					else
 					{
-						//call player.move(match.Groups[1], match.Groups[2]);
+						match = Regex.Match(input, m_turnString);
+						if (match.Success)
+						{
+							//call player.turn(match.Groups[1]);
+							input += "...";
+						}
+						else
+						{
+							match = Regex.Match(input, m_moveString);
+							if (match.Success)
+							{
+								if (match.Groups.Count == 5)
+								{
+									//call player.move(match.Groups[1], match.Groups[2], match.Groups[3]);
+									//input += Msg;
+								}
+								else
+								{
+									//call player.move(match.Groups[1], match.Groups[2]);
+									//input += Msg;
+								}
+							}
+							else
+							{
+								input += ", order not recongnixed";
+							}
+						}
 					}
 				}
 
+				m_log.text += "\n" + input;
+				m_input.textComponent.text = "";
 			}
 		}
 	}
-}
