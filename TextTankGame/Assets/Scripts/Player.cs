0c9e6004b265cb8e488b6fc151d1c451d7e3ed8b
Unknown <brbodily@student.neumont.edu>
2018-04-20 22:00:56 -0600

    Setting up game and player classes

---
+++ TextTankGame/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] [Range(0.0f, 1000.0f)] float m_projectileSpeed = 5.0f;
+    [SerializeField] [Range(0.0f, 90.0f)] float m_angle = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
