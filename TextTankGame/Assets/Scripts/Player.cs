949318913f52424694ae601a39728b29e724759b
Unknown <brbodily@student.neumont.edu>
2018-04-21 22:46:23 -0600

    Actual methods for player

--- TextTankGame/Assets/Scripts/Player.cs
+++ TextTankGame/Assets/Scripts/Player.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Player : Tank
 {
+    [SerializeField] Console m_console = null;
+    [SerializeField] float m_closeHit = 1.0f;
+    [SerializeField] float m_inVicinity = 3.0f;
 
-	// Use this for initialization
-	void Start()
+	public override GameObject Fire()
 	{
+        GameObject result = base.Fire();
 
         Tank enemy = result.GetComponent<Tank>();
         float distance = (result.transform.position - transform.position).magnitude;
@@ -38,9 +41,71 @@ public class Player : Tank
         return result;
     }
 
-	// Update is called once per frame
-	void Update()
-	{
+    public override void Spawn()
+    {
+        Console.Msg = "Get those fingers preped and ready! An enemy tank has appeared!";
+        m_console.LogMessage();
+        base.Spawn();
+    }
 
-	}
+    public override void Died()
+    {
+        Console.Msg = "What kind of tank commander are you?!? We're more like swiss cheese than a tank.";
+        m_console.LogMessage();
+        base.Died();
+    }
+
+    public override void Hit(float damage)
+    {
+        Console.Msg = "We've been hit! Take them down quickly";
+        m_console.LogMessage();
+        base.Hit(damage);
+    }
+
+    public override void Collision()
+    {
+        Console.Msg = "You've slammed into something! You're lucky we don't have any new holes!";
+        base.Collision();
+    }
+
+    public void Loot(int deadTankNum)
+    {
+
+    }
+
+    public void Retreat(int distance)
+    {
+
+    }
+
+    public void Retreat(int distance, string speed)
+    {
+
+    }
+
+    public void Move(string direction, int distance)
+    {
+        
+    }
+
+    public void Move(string direction, int distance, string speed)
+    {
+        
+    }
+
+    public void Scan()
+    {
+
+    }
+
+    public void Turn(int angle)
+    {
+
+    }
+
+    public void Angle(int angle)
+    {
+
+    }
+    //loot, move, retreat, scan, turn, angle, 
 }
