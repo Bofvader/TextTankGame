34ce9dac42e6d34861c9f5511a7934552262d56d
bdorman <bdorman@student.neumont.edu>
2018-04-23 13:37:10 -0600

    Fixed the ai firing

--- TextTankGame/Assets/Scripts/AI.cs
+++ TextTankGame/Assets/Scripts/AI.cs
@@ -15,20 +15,6 @@ public class AI : Tank
 	float m_updateAngle = 0.0f;
 	Vector3 m_path = Vector3.forward;
 
-    public void Start()
-    {
-        SetRange();
-    }
-
-    public void SetRange()
-    {
-        m_range = Mathf.Pow(m_projectileSpeed, 2);
-        float launch = Mathf.Deg2Rad * 45 /*m_tiltAngle*/;
-
-        m_range *= Mathf.Sin(launch);
-        m_range /= Game.Instance.Gravity;
-    }
-
 	protected void Update()
 	{
 		if (Alive)
@@ -40,8 +26,6 @@ public class AI : Tank
 
 			if(InRangeOfTarget())
 			{
-				m_isFiring = true;
-
 				Debug.Log("Within range");
 
 				Fire();
@@ -135,17 +119,27 @@ public class AI : Tank
 
 		if(ShotReady)
 		{
-			StartCoroutine(TravelTime(distance, offset));
-
-			if(m_wasHit)
+			if (!m_isFiring)
 			{
-				m_wasHit = false;
-				go = m_target;
+				m_shotTimer = 0.0f;
 
-				Tank t = m_target.GetComponent<Tank>();
-				if(t)
+				Debug.Log("Is firing");
+				m_isFiring = true;
+				PlayGunSound();
+				StartCoroutine(TravelTime(distance, offset));
+
+				if (m_wasHit)
 				{
-					t.Hit(m_damage);
+					Debug.Log("Target was hit");
+
+					m_wasHit = false;
+					go = m_target;
+
+					Tank t = m_target.GetComponent<Tank>();
+					if (t)
+					{
+						t.Hit(m_damage);
+					}
 				}
 			}
 		}
@@ -155,9 +149,13 @@ public class AI : Tank
 
 	IEnumerator TravelTime(float distance, Vector3 targetPosition)
 	{
-		float time = distance * m_projectileSpeed;
+		float time = distance / m_projectileSpeed;
+
+		Debug.Log("Start travel");
 
 		yield return new WaitForSeconds(time);
+
+		Debug.Log("End travel");
 
 		Vector3 actual = m_target.transform.position - transform.position;
 		float difference = (actual - targetPosition).magnitude;
@@ -171,5 +169,7 @@ public class AI : Tank
 		{
 			m_wasHit = true;
 		}
+
+		m_isFiring = false;
 	}
 }
